ee48d95e7f846c04d5dbad3a82cd07d17c42eb61
moldcraft <moldcraft@email.com>
2014-11-26 18:53:24 +0200

    Added the update parameter

--- manifest.php
+++ manifest.php
@@ -9,3 +9,5 @@ $manifest['description'] = __( "This extension lets you control the website visu
 $manifest['version']     = '1.0.0';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
+
+$manifest['github_update'] = 'ThemeFuse/Unyson-Styling-Extension';
