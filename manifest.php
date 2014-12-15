2fd7945adcbb203592c17a6c4e3b3bb1ab67dd73
Valeriu <valeriuzdrobau@gmail.com>
2014-12-15 15:07:23 +0200

    version 1.0.2

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Styling', 'fw' );
 $manifest['description'] = __( "This extension lets you control the website visual style. Starting from predefined styles to changing specific fonts and colors across the website.", 'fw' );
-$manifest['version']     = '1.0.1';
+$manifest['version']     = '1.0.2';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
