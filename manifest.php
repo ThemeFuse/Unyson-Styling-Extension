f595c2ddd9cec31097ebe868d382e2407896da4d
moldcraft <moldcraft@email.com>
2016-09-07 19:04:32 +0300

    Version 1.0.6

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Styling', 'fw' );
 $manifest['description'] = __( "This extension lets you control the website visual style. Starting from predefined styles to changing specific fonts and colors across the website.", 'fw' );
-$manifest['version']     = '1.0.7';
+$manifest['version']     = '1.0.6';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
